fc4ecf091b5f622d7ef96612dec2b5db932b8615
ADITYA <75035219+DEVLOPRR@users.noreply.github.com>
2022-01-24 06:21:08 +0530

    Update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -17,7 +17,7 @@ function serve() {
 	return {
 		writeBundle() {
 			if (server) return;
-			server = require('child_process').spawn('npm', ['run', 'start'], {
+			server = require('child_process').spawn('npm', ['run', 'start:neu'], {
 				stdio: ['ignore', 'inherit', 'inherit'],
 				shell: true
 			});
